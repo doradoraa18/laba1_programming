825637f15c65683a91231bb6f1910ca9651f5422
doradoraa18 <orlovadaria18@mail.ru>
2025-11-28 22:17:37 +0300

    Add custom exceptions

---
+++ exceptions.py
@@ -0,0 +1,17 @@
+﻿class BookStoreException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class FileOperationException(BookStoreException):
+    def __init__(self, message, filename):
+        self.filename = filename
+        super().__init__(f"{message} (Файл: {filename})")
+
+class BookNotFoundException(BookStoreException):
+    def __init__(self, book_id):
+        super().__init__(f"Книга с ID {book_id} не найдена")
+
+class ValidationException(BookStoreException):
+    def __init__(self, field, value):
+        super().__init__(f"Некорректное значение '{value}' для поля '{field}'")
